1be87cfd5a06ef6f44e6f97317c65de4812d052f
AgostoSound <agostosound@gmail.com>
2023-12-09 16:11:41 -0300

    Crea función para envío de mensaje, abre la pestaña de Whatsapp web y escribe el mensaje en el chat correcto pero no lo envía.

--- api/send_message.py
+++ api/send_message.py
@@ -1,2 +1,31 @@
-import json
+import pywhatkit
+from datetime import datetime, timedelta
 
+num = '+5493517538429'  # Número de destino del mensaje.
+msg = '8==D'  # Mensaje.
+wait_time = 1  # Tiempo de espera para enviar el mensaje.
+add_time = 10  # Para buscar la hora y minuto a la que se envía el mensaje lo toma 5 segundos luego de la hora actual.
+
+
+def send_a_message():
+    """
+    Envía un mensaje por Whatsapp.
+    """
+    try:
+        now = datetime.now()
+        next_hour = now + timedelta(seconds=add_time)
+        hour, minute = next_hour.time().hour, next_hour.time().minute
+
+        print(' ')
+        print(f'Hora: {hour} - tipo: {type(hour)} ')
+        print(f'Minuto: {minute} - tipo: {type(minute)}')
+        print(' ')
+
+        pywhatkit.sendwhatmsg(num, msg, hour, minute, wait_time=wait_time)
+
+        return 'Mensaje enviado.'
+    except Exception as e:
+        return str(e)
+
+
+send_a_message()
